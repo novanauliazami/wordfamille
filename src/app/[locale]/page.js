9e5116de3cafb10f277eb8661c1f4273bfb8147b
novanauliazami <novanauliazami@gmail.com>
2023-11-19 15:58:26 +0700

    Tambah menu

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,11 +1,13 @@
 'use client'
 
 import Hero from '@/components/hero'
+import Menus from '@/components/menus'
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Menus />
     </main>
   )
 }
